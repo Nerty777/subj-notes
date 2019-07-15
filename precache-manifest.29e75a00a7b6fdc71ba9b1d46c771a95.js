4b88beefa323be5007d246134d0c89abee56969f
Ivan <nerty777@gmail.com>
2019-07-15 14:04:28 +0300

    Updates

---
+++ precache-manifest.29e75a00a7b6fdc71ba9b1d46c771a95.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2348b5516573f580178a7f32dfc2a1af",
+    "url": "/subj-notes/index.html"
+  },
+  {
+    "revision": "8e58529399be20bfa2f5",
+    "url": "/subj-notes/static/css/main.61a78b81.chunk.css"
+  },
+  {
+    "revision": "426037134f798f255462",
+    "url": "/subj-notes/static/js/2.baa383f0.chunk.js"
+  },
+  {
+    "revision": "8e58529399be20bfa2f5",
+    "url": "/subj-notes/static/js/main.eb802a39.chunk.js"
+  },
+  {
+    "revision": "2e8405087ca0ff4a8df4",
+    "url": "/subj-notes/static/js/runtime~main.29bc6227.js"
+  }
+]);
